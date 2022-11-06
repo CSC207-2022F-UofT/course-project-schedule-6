ebee5df0ca2827a506af35fef7f7c4abaaf850c2
nizarabdouss <nizarabdouss@gmail.com>
2022-11-06 16:57:01 -0500

    User constructor, getter and setter methods

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,5 +1,34 @@
 package entity;
 
 public class User {
+    private String userName;
+    private String email;
+    private String name;
+
+    public User(String name, String userName, String email) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 
 }
